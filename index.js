cc4fbb2fc1f879c271216f9d2e90f34e493434d3
Giovanni Schroevers <giovie2000@hotmail.com>
2022-01-28 16:40:33 +0100

    promisify exec

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const util = require('util');
-const exec = util.promisify(require("child_process").exec);
+const exec = util.promisify(require('child_process').exec);
 const { WebClient } = require('@slack/web-api');
 
 async function run() {
   const web = new WebClient(core.getInput('slack-token'));
-  const {error, stdout, stderr} =  await exec("url=https://www.12gobiking.nl/ prod=1 npx playwright test compare-servers.test.ts")
+  const {stdout, stderr} = await exec("url=https://www.12gobiking.nl/ prod=1 npx playwright test compare-servers.test.ts")
 
-  if (error) {
-    core.setFailed(error.message)
+  if (stderr) {
+    core.setFailed(stderr)
     await web.chat.postMessage({
-      text: error.message,
+      text: stderr,
       channel: core.getInput('slack-channel'),
     });
   } else {
