3e18613665109267c99d15256d1f01509258df0e
Giovanni Schroevers <giovie2000@hotmail.com>
2022-01-28 15:05:02 +0100

    send slack message with test result

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const { exec } = require("child_process");
+const { WebClient } = require('@slack/web-api');
 
 async function run() {
-  exec("url=https://www.12gobiking.nl/ prod=1 npx playwright test", ((error, stdout, stderr) => {
+  let testResults = ''
+  exec("url=https://www.12gobiking.nl/ prod=1 npx playwright test compare-servers.test.ts", ((error, stdout, stderr) => {
+    const web = new WebClient(core.getInput('slack-token'));
+
     if (error) {
       core.setFailed(error.message)
       console.log(`Error: ${error.message}`)
     }
-    console.log(`stdout: ${stdout}`);
+
+    if (stderr) {
+      core.setFailed(stderr)
+    }
+
+    testResults = stdout
     core.setOutput("result", stdout)
   }))
+  const result = await web.chat.postMessage({
+    text: testResults,
+    channel: core.getInput('slack-channel'),
+  });
 }
 
 run()
