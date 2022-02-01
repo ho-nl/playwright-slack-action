e6e041785d63ace851dd2d27128d5b76c8941fb2
Giovanni Schroevers <giovie2000@hotmail.com>
2022-02-01 16:29:25 +0100

    feat(GOM2-1215): add warning message

--- index.js
+++ index.js
@@ -10,19 +10,23 @@ async function run() {
   let output = ''
   let failedTestOutput = ''
   let failedTests = 0
+  let warnings = 0
 
   test.stdout.on('data', data => {
-    output += data
+    output += data + '\n'
     data = data.toString()
     if (data.includes(`${failedTests + 1})`)) {
       data = data.slice(0, data.search(/\[2m/))
       data = data.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '')
       failedTestOutput += data + '\n'
       failedTests++
+    } else if (data.includes('Warning:')) {
+      failedTestOutput += data + '\n'
+      warnings += 1
     }
   });
 
-  test.on('close', async code => {
+  test.on('close', async () => {
     if (failedTests) {
       const message = await web.chat.postMessage({
         attachments: [{'color': '#cc0000', 'text': `${failedTests} Tests failed :x:`, fallback: `${failedTests} Tests failed :x:`}],
@@ -44,6 +48,16 @@ async function run() {
         })
         core.setFailed(output)
       }
+    } else if(warnings) {
+      const message = await web.chat.postMessage({
+        attachments: [{'color': '#cc0000', 'text': `Tests passed with ${warnings} warnings :warning:`, fallback: `Tests passed with ${warnings} warnings :warning:`}],
+        channel: core.getInput('slack-channel'),
+      });
+      await web.chat.postMessage({
+        text: failedTestOutput,
+        channel: core.getInput('slack-channel'),
+        thread_ts: message.ts
+      })
     } else {
       await web.chat.postMessage({
         attachments: [{'color': '#36a64f', 'text': 'All tests passed :white_check_mark:', fallback: 'All tests passed :white_check_mark:'}],
