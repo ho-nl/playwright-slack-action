9e232a5cd54d54ffa4585b95d529d127bb7566bb
Giovanni Schroevers <giovie2000@hotmail.com>
2022-02-02 10:35:34 +0100

    feat(GOM2-1215): strip code segment from error message

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ async function run() {
   let warnings = 0
 
   test.stdout.on('data', data => {
-    core.info(data)
+    output += data
     data = data.toString()
     if (data.includes(`${failedTests + 1})`)) {
-      data = data.slice(0, data.search(/\[2m/))
+      data = data.slice(0, data.search(/[1-9]{1,3} \|/)).trim()
       data = data.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '')
       failedTestOutput += data + '\n'
       failedTests++
@@ -41,12 +41,12 @@ async function run() {
         const files = fs.readdirSync('./comparison');
         files.forEach((filename) => {
           web.files.upload({
+            file: fs.createReadStream('./comparison/' + filename),
             channels: core.getInput('slack-channel'),
-            thread_ts: message.ts,
-            file: fs.createReadStream('./comparison/' + filename)
+            thread_ts: message.ts
           })
         })
-        core.setFailed('')
+        core.setFailed(output.toString())
       }
     } else if(warnings) {
       const message = await web.chat.postMessage({
@@ -58,11 +58,13 @@ async function run() {
         channel: core.getInput('slack-channel'),
         thread_ts: message.ts
       })
+      core.info(output.toString())
     } else {
       await web.chat.postMessage({
         attachments: [{'color': '#36a64f', 'text': 'All tests passed :white_check_mark:', fallback: 'All tests passed :white_check_mark:'}],
         channel: core.getInput('slack-channel'),
       });
+      core.info(output.toString())
     }
   });
 }
