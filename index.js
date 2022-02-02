3109091a00ada88d190acd9a78cb0efc5bb14383
Giovanni Schroevers <33447742+Giovanni-Schroevers@users.noreply.github.com>
2022-02-02 13:14:45 +0100

    feat(GOM2-1215): send more detailed slack message for e2e tests (#1)
    * initial commit
    
    * add url and prod parameter
    
    * set node to 12
    
    * add package.json
    
    * run run
    
    * add node modules to repo
    
    * send slack message with test result
    
    * send slack message with test result
    
    * add slack node modules
    
    * wait for result of test before sending message
    
    * promisify exec
    
    * promisify exec
    
    * send slack message in then
    
    * feat(GOM2-1215): send error, server comparison screenshots when tests fail
    
    * feat(GOM2-1215): add warning message
    
    * feat(GOM2-1215): log data
    
    * feat(GOM2-1215): log data before toString
    
    * feat(GOM2-1215): strip code segment from error message
    
    * feat(GOM2-1215): test output
    
    * feat(GOM2-1215): test output
    
    * feat(GOM2-1215): test output
    
    * feat(GOM2-1215): test output
    
    * feat(GOM2-1215): set fail message

---
+++ index.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+const { spawn } = require('child_process');
+
+const core = require('@actions/core');
+const { WebClient } = require('@slack/web-api');
+
+async function run() {
+  const web = new WebClient(core.getInput('slack-token'));
+  const test = spawn('npx', ['playwright', 'test'], { env: { ...process.env, URL: core.getInput('url'), PROD: core.getInput('prod') }});
+  let output = ''
+  let failedTestOutput = ''
+  let failedTests = 0
+  let warnings = 0
+
+  test.stdout.on('data', data => {
+    output += data
+    data = data.toString()
+    if (data.includes(`${failedTests + 1})`)) {
+      data = data.slice(0, data.search(/[1-9]{1,3} \|/)).trim()
+      data = data.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '')
+      failedTestOutput += data + '\n'
+      failedTests++
+    } else if (data.includes('Warning:')) {
+      failedTestOutput += data + '\n'
+      warnings += 1
+    }
+  });
+
+  test.on('close', async () => {
+    if (failedTests) {
+      const message = await web.chat.postMessage({
+        attachments: [{'color': '#cc0000', 'text': `${failedTests} Tests failed :x:`, fallback: `${failedTests} Tests failed :x:`}],
+        channel: core.getInput('slack-channel'),
+      });
+      await web.chat.postMessage({
+        text: failedTestOutput,
+        channel: core.getInput('slack-channel'),
+        thread_ts: message.ts
+      })
+      if (fs.existsSync('./comparison')) {
+        const files = fs.readdirSync('./comparison');
+        files.forEach((filename) => {
+          web.files.upload({
+            file: fs.createReadStream('./comparison/' + filename),
+            channels: core.getInput('slack-channel'),
+            thread_ts: message.ts
+          })
+        })
+      }
+      core.info(output)
+      core.setFailed('Tests failed')
+    } else if(warnings) {
+      const message = await web.chat.postMessage({
+        attachments: [{'color': '#ffcc00', 'text': `Tests passed with ${warnings} warnings :warning:`, fallback: `Tests passed with ${warnings} warnings :warning:`}],
+        channel: core.getInput('slack-channel'),
+      });
+      await web.chat.postMessage({
+        text: failedTestOutput,
+        channel: core.getInput('slack-channel'),
+        thread_ts: message.ts
+      })
+      core.info(output)
+    } else {
+      await web.chat.postMessage({
+        attachments: [{'color': '#36a64f', 'text': 'All tests passed :white_check_mark:', fallback: 'All tests passed :white_check_mark:'}],
+        channel: core.getInput('slack-channel'),
+      });
+      core.info(output)
+    }
+  });
+}
+
+run()
